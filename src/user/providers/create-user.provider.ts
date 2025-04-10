034f5cee40892034475a257968256ef8a1ab5f9e
Abiola Dave <abioladave24@gmail.com>
2025-04-10 15:00:49 +0100

    Update create-user.provider.ts

--- src/user/providers/create-user.provider.ts
+++ src/user/providers/create-user.provider.ts
@@ -46,26 +46,26 @@ export class CreateUserProvider {
       );
     }
 
-    // Generate a unique account number
+   
     let accountNumber: string;
     let exists: User | undefined;
 
     do {
       accountNumber = Math.floor(
         1000000000 + Math.random() * 9000000000,
-      ).toString(); // 10-digit number
+      ).toString(); 
       exists = await this.userRepository.findOne({ where: { accountNumber } });
     } while (exists);
 
     console.log('DTO:', createUserDto);
 
-    // Create and populate the new user entity
+    
     const newUser = this.userRepository.create({
       ...createUserDto,
       password: await this.hashingProvider.hashedPassword(
         createUserDto.password,
       ),
-      accountNumber, // ✅ Don't forget this
+      accountNumber, 
     });
 
     try {
